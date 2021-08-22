9bb8c569b80f84ae884a5640c17f468eb4ce18b7
Tom McDonnell <tomcdonnell@gmail.com>
2021-08-22 15:00:01 +1000

    Latest changes.

--- js_scratchpad.js
+++ js_scratchpad.js
@@ -10,6 +10,19 @@ window.maxAndMinValueByColumnKey =
    percentNotInEducationOther           : {max:     10, min:     0}
 };
 
+window.maxAndMinValueByColumnKey =
+{
+   avgTaxableIncomePerTaxpayer           : {max: 445904, min: -9778},
+   avgSalaryOrWagesPerTaxpayer           : {max: 102783, min: 11301},
+//   avgNonSalaryOrWagesPerTaxpayer        : {max: , min: },
+   avgHelpDebtDollarsPerTaxpayer         : {max:    812, min:     0},
+   avgGovAllowancesAndPaymentsPerTaxpayer: {max:   3906, min:     0},
+   avgGovPensionsAndAllowancesPerTaxpayer: {max:   2851, min:     0},
+   avgSuperIncomePerTaxpayer             : {max:   9656, min:     0},
+   avgGiftsOrDonationsPerTaxpayer        : {max: 103305, min:     3},
+   avgTotalDeductionsPerTaxpayer         : {max: 109377, min:   246}
+};
+
 $(document).ready
 (
    function ()
@@ -20,6 +33,8 @@ $(document).ready
 
 function _setBackgroundColorsForColumnsWithMinAndMaxValues()
 {
+   boolAddColorKey = false;
+
    for (var columnKey in window.maxAndMinValueByColumnKey)
    {
       var minAndMax   = window.maxAndMinValueByColumnKey[columnKey];
@@ -36,6 +51,8 @@ function _setBackgroundColorsForColumnsWithMinAndMaxValues()
          }
       }
 
+      boolAddColorKey = true;
+
       if (columnIndex === null)
       {
          return;
@@ -51,6 +68,11 @@ function _setBackgroundColorsForColumnsWithMinAndMaxValues()
          tdJq.css('backgroundColor', _getCssColorForRangeValue(minAndMax.min, tdVal, minAndMax.max));
       }
    }
+
+   if (boolAddColorKey)
+   {
+      _addColorKeyDiv();
+   }
 }
 
 function _getCssColorForRangeValue(rangeMin, value, rangeMax)
@@ -65,10 +87,34 @@ function _getCssColorForRangeValue(rangeMin, value, rangeMax)
    if (value > rangeMax) {return 'rgb(' + finalR   + ',' + finalG   + ',' + finalB   + ')';}
 
    var valueAsFraction = (value - rangeMin) / (rangeMax - rangeMin);
-
    var r = Math.round(initialR + (finalR - initialR) * (valueAsFraction / 0.5), 0);
    var g = Math.round(initialG + (finalG - initialG) * (valueAsFraction / 0.5), 0);
    var b = Math.round(initialB + (finalB - initialB) * (valueAsFraction / 0.5), 0);
 
    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function _addColorKeyDiv()
+{
+   $(
+      '<div class="difference-color-key-div">' +
+       '<table style="width:100%">' +
+        '<tbody>' +
+         '<tr>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 9, 10) + ';text-align:right;">High value</td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 8, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 7, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 6, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 5, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 4, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 3, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 2, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 1, 10) + ';"></td>' +
+          '<td style="height:20px;width:10%;background-color:' + _getCssColorForRangeValue(0, 0, 10) + ';">Low value</td>' +
+        '</tr>' +
+       '</tbody>' +
+      '</table>' +
+      '<div class="clear-floats"></div>' +
+     '</div>'
+   ).insertBefore('div#editable-table-search-form-container');
 }
